231749be3efd93e8dc2efd1d962df6bab5a8ca43
kenchiwar <huynhtienthang1264@gmail.com>
2023-07-08 16:15:07 +0700

    kohaylamkohaylam

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ABCComponent } from './abc.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'abc', component:ABCComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'YourProject'`, () => {
+  it(`should have as title 'ProjectAngularNodejs'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('YourProject');
+    expect(app.title).toEqual('ProjectAngularNodejs');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('YourProject app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ProjectAngularNodejs app is running!');
   });
 });
