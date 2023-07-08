07938fbc9a1853d966e807197c887b1d22a0497f
LeToaan <thaitoan553@gmail.com>
2023-07-08 15:06:14 +0700

    first commit

---
+++ src/app/abc.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    templateUrl: 'abc.component.html'
+})
+
+export class ABCComponent implements OnInit{
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+    
+}
